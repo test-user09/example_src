9c4e049897e3f892f449d1d3e731ab9e9246a8ea
test-user09 <138655368+test-user09@users.noreply.github.com>
2023-07-05 10:07:31 +0000

    added machine user

--- pipeline/deploy.sh
+++ pipeline/deploy.sh
@@ -4,13 +4,14 @@ set -e
 echo "start  $0 {"
 
 rm -rf example_webserver/ 
-git clone https://github.com/test-user09/example_webserver.git
+git clone "https://$MACHINE_USER_TOKEN@github.com/test-user09/example_webserver.git"
 
 cd example_webserver/
 cp ../index.html .
 git add --all 
-git commit -m "$(date)"
+git commit -m "$(date)" || true 
 git push 
 
+cd ..
 rm -rf example_webserver/ 
 echo "end  $0 }"
